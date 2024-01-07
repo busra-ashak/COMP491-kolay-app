f7ab4611cbaa2f585920390d5898d53409cec909
emirhanerel <72880487+emirhanerel@users.noreply.github.com>
2024-01-07 22:54:37 +0300

    log in and sign up pages implemented & profile page filled

--- kolay_app/lib/screens/profile.dart
+++ kolay_app/lib/screens/profile.dart
@@ -1,7 +1,12 @@
 // DrawerPage.dart
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kolay_app/screens/log_in.dart';
+import 'package:kolay_app/service/firebase_auth_services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -9,6 +14,38 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  late String email = '';
+  late String name = '';
+  late String photoURL = '';
+  User? _user;
+  @override
+  void initState() {
+    super.initState();
+    _user = _auth.currentUser;
+    _loadUserData();
+  }
+
+  _loadUserData() async {
+    if (_user != null) {
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('USERS').doc(_user!.uid).get();
+
+      if (snapshot.exists) {
+        Map<String, dynamic> userData = snapshot.data()!;
+        // Now you can use userData to populate your UI
+        setState(() {
+          // Update UI with user data
+          // e.g., assign values to variables that are bound to your UI widgets
+          email = userData['email'];
+          name = userData['name'];
+          photoURL = userData['photoURL'];
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,16 +65,13 @@ class _ProfilePageState extends State<ProfilePage> {
         padding: EdgeInsets.zero,
         children: [
           UserAccountsDrawerHeader(
-            accountName: const Text('Jane Doe'),
-            accountEmail: const Text('example@gmail.com'),
+            accountName: Text(name),
+            accountEmail: Text(email),
             currentAccountPicture: CircleAvatar(
               child: ClipOval(
-                child: Image.network(
-                  'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
-                  fit: BoxFit.cover,
-                  width: 90,
-                  height: 90,
-                ),
+                child: photoURL.isNotEmpty
+                    ? Image.file(File(photoURL))
+                    : Image.asset('lib/assets/default_profile_picture.jpg'),
               ),
             ),
             decoration: const BoxDecoration(
@@ -50,11 +84,13 @@ class _ProfilePageState extends State<ProfilePage> {
           ListTile(
             title: const Text('Log Out'),
             leading: const Icon(Icons.exit_to_app),
-            onTap: (){
+            onTap: () {
               FirebaseAuth.instance.signOut();
               Navigator.pushAndRemoveUntil(
-                            context, MaterialPageRoute(builder: (context) => LoginPage()), (route) => false);
-              },
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                  (route) => false);
+            },
           ),
         ],
       ),
